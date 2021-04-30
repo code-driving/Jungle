c4ac587c43c81c3e74f63ce995b13bcf1200dbb2
code-driving <tittan7@gmail.com>
2021-04-30 01:00:18 +0000

    Finish feature user auth. Start work on EDD Rails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,19 @@ class UsersController < ApplicationController
     end
   end
   
+  # def create
+  #   user = User.new(user_params)
+
+  #   if user.save
+  #     session[:user_id] = user.id
+  #     redirect_to '/', notice: 'Account created successfully'
+  #   else
+  #     flash[:error] = 'An error occured!'
+  #     render 'new'
+  #   end
+  # end
+  
+  
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
