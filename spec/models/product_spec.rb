7dcd2b31abd85b28b89cb564e27b7e5d9862ea40
code-driving <tittan7@gmail.com>
2021-05-03 20:23:19 +0000

    Continue work on testing product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,88 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+    it "ensures the presence of all four fields: name, price, quantity, category" do
+      @category = Category.new
+      @category.id = 20
+      @category.name = "products"
+      
+      @product = Product.new
+      @product.name = "sofa"
+      @product.price = 2000
+      @product.quantity = 50
+      @product.category_id = @category
+      
+      @category.products = [@product]
+      @product.save
+      
+      expect(@product).to be_valid
+    end
+  
+    it 'ensures the presence of name' do
+      @category = Category.new
+      @category.id = 20
+      @category.name = "products"
+
+      @product = Product.new
+      @product.name = nil
+      @product.price = 2000
+      @product.quantity = 50
+      @product.category_id = @category
+
+      @category.products = [@product]
+      @product.save
+
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages.length).to eql(1)
+      expect(@product.errors.full_messages[0]).to eql("Name can't be blank")
+    end 
+
+    it 'ensures the presence of price' do
+      @category = Category.new
+      @category.id = 20
+      @category.name = "products"
+
+      @product = Product.new
+      @product.name = "sofa"
+      @product.price = nil
+      @product.quantity = 50
+      @product.category_id = @category
+
+      @category.products = [@product]
+      @product.save
+
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages[0]).to eql("Price cents is not a number")
+    end 
+
+    it 'ensures the presence of quantity' do
+      @category = Category.new
+      @category.id = 20
+      @category.name = "products"
+
+      @product = Product.new
+      @product.name = "sofa"
+      @product.price = 2000
+      @product.quantity = nil
+      @product.category_id = @category
+
+      @category.products = [@product]
+      @product.save
+
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages[0]).to eql("Quantity can't be blank")
+    end 
+
+    it 'should have a category' do
+      @category = nil     
+      @product = Product.new
+      @product.name = "sofa"
+      @product.price = 2000
+      @product.quantity = 50
+      @product.category_id = nil  
+      @product.save
+      expect(@product.category).to be_nil
+      expect(@product.errors.full_messages[0]).to eql("Category can't be blank")
+    end 
   end
 end
